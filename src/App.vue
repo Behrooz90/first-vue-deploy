e39193ee1e57536e530bca91627604ee0582b494
bdinyarian <bdinyarian@knox.edu>
2021-03-28 22:16:23 -0500

    added picture of me

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Behrooz Picture" src="./assets/picture_of_behrooz.png">
+    <HelloWorld msg="Welcome to Behrooz's Vue.js App"/>
   </div>
 </template>
 
